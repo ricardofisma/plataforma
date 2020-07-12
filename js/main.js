ce5f0bd28562e18fe9b6921fe062000670a47e46
ricardofisma <ricardomallqui6@gmail.com>
2020-07-11 21:46:39 -0500

    w

---
+++ js/main.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+  // MOSTRANDO Y OCULTANDO MENU
+  $("#button-menu").click(function () {
+    if ($("#button-menu").attr("class") == "fa fa-bars") {
+      $(".navegacion").css({ width: "100%", background: "rgba(0,0,0,.5)" }); // Mostramos al fondo transparente
+      $("#button-menu").removeClass("fa fa-bars").addClass("fa fa-arrow-left"); // Agregamos el icono X
+      $(".navegacion .menu").css({ left: "0px" }); // Mostramos el menu
+    } else {
+      $(".navegacion").css({ width: "0%", background: "rgba(0,0,0,.0)" }); // Ocultamos el fonto transparente
+      $("#button-menu").removeClass("fa fa-arrow-left").addClass("fa fa-bars"); // Agregamos el icono del Menu
+      $(".navegacion .submenu").css({ left: "-320px" }); // Ocultamos los submenus
+      $(".navegacion .menu").css({ left: "-320px" }); // Ocultamos el Menu
+    }
+  });
+
+  // MOSTRANDO SUBMENU
+  $(".navegacion .menu > .item-submenu a").click(function () {
+    var positionMenu = $(this).parent().attr("menu"); // Buscamos el valor del atributo menu y lo guardamos en una variable
+    console.log(positionMenu);
+
+    $(".item-submenu[menu=" + positionMenu + "] .submenu").css({ left: "0px" }); // Mostramos El submenu correspondiente
+  });
+
+  // OCULTANDO SUBMENU
+  $(".navegacion .submenu li.go-back").click(function () {
+    $(this).parent().css({ left: "-320px" }); // Ocultamos el submenu
+  });
+
+  // MOSTRANDO SUBMENU
+  $(".itemw-submenuw a").click(function () {
+    var positionMenuw = $(this).parent().attr("menu");
+    console.log(positionMenuw);
+
+    $(".itemw-submenuw[menu=" + positionMenuw + "] .submenuw").css({
+      left: "0px",
+    });
+  });
+
+  // OCULTANDO SUBMENU
+  $(".go-backw").click(function () {
+    $(this).parent().css({ left: "-320px" }); // Ocultamos el submenu
+  });
+});
