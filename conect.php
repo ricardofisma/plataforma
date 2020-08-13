46900b710b3dff549e84a33d91ffee49a18bb474
ricardofisma <ricardomallqui6@gmail.com>
2020-08-13 07:57:39 -0500

    w

--- conect.php
+++ conect.php
@@ -1,6 +1,6 @@
 <?php
-$link = mysqli_connect("localhost", "root", "","datos");
-//$link = mysqli_connect("b2fl5juvonx7jy2herss-mysql.services.clever-cloud.com", "u2torrdwkgyxog4s", "PPe6V7lNx0FjMHYfApZI","b2fl5juvonx7jy2herss");
+//$link = mysqli_connect("localhost", "root", "","datos");
+$link = mysqli_connect("b2fl5juvonx7jy2herss-mysql.services.clever-cloud.com", "u2torrdwkgyxog4s", "PPe6V7lNx0FjMHYfApZI","b2fl5juvonx7jy2herss");
 //$link = mysqli_connect("sql213.byethost.com", "b11_25901687", "@Qq1w2e3r4t5y6","b11_25901687_datos");
 
 //   ftp.byethost.com
